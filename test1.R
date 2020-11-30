b7f4b59f97c9792937cb1e3999c97ac948104f68
armaan10 <33001323+armaan10@users.noreply.github.com>
2020-11-30 10:57:06 +0530

    Add files via upload

---
+++ test1.R
@@ -0,0 +1,21 @@
+#get current working dir
+getwd()
+x=1
+mode(x)
+str1<-"sdasd"
+mode(str1)
+length(str1)
+cat(str1)
+vec<-c(1,2,3,4)
+mode(vec)
+x<-c(1:30)
+x<-x-1
+x
+help("sequence")
+#create vars v1 to v3
+myvars<-paste("v",1:3,sep="")
+myvars
+vec1<-1:4
+vec2<-2:5
+if(vec1>vec2) print("yes")else print("no")
+
